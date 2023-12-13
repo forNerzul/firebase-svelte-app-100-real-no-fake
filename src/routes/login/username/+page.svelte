14b553531c590d0e7e1dc52786dadbcb6acf3325
forNerzul <sbeardman92@gmail.com>
2023-12-13 19:30:45 -0300

    Agregada funcionalidad para verificar la disponibilidad y confirmación de nombres de usuario
    - Importado componente AuthCheck para control de autenticación
    - Importados módulos necesarios de Firebase para operaciones de base de datos
    - Inicializadas variables para nombre de usuario, estado de carga y estado de disponibilidad
    - Implementada una función de debounce para verificar la disponibilidad del nombre de usuario
    - Creada lógica para verificar si un nombre de usuario existe en la base de datos
    - Integrada la función checkAvailability con el evento de entrada para validar el nombre de usuario en tiempo real
    - Implementada parcialmente la función confirmUsername para la confirmación del nombre de usuario

--- src/routes/login/username/+page.svelte
+++ src/routes/login/username/+page.svelte
@@ -1,4 +1,41 @@
 <script lang="ts">
-</script>
+    import AuthCheck from "$lib/components/AuthCheck.svelte";
+    import {db, user } from '$lib/firebase'
+    import {doc, getDoc, writeBatch} from 'firebase/firestore'
 
-<h2>Username</h2>
+    let username = "";
+    let loading = false;
+    let isAvailable = false;
+
+    let debounceTimer: NodeJS.Timeout;
+
+    async function checkAvailability(){
+        isAvailable = false;
+        clearTimeout(debounceTimer);
+
+        loading = true;
+
+        debounceTimer = setTimeout( async () => {
+            console.log('checking availability of ', username)
+
+            const ref = doc(db, "usernames", username);
+            const exist = await getDoc(ref).then((doc) => doc.exists());
+
+            isAvailable = !exist;
+            loading = false;
+
+            }, 500);
+        };
+
+        async function confirmUsername(){
+            // TODO: 
+        }
+</script>
+<AuthCheck>
+    <h2>Username</h2>
+    <form class="w-2/5" on:submit|preventDefault={confirmUsername}>
+        <input type="text" placeholder="Username" class="w-full input" bind:value={username} on:input={checkAvailability}>
+        <p>Is Available? {isAvailable}</p>
+        <button class="btn btn-success">Confirm username @{username}</button>
+    </form>
+</AuthCheck>
