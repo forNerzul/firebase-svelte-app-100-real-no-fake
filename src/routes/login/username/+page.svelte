0757254285c6ac5d54d84f01a8bbcc92ce295294
forNerzul <sbeardman92@gmail.com>
2023-12-14 11:44:21 -0300

    Agregada lógica para confirmar y guardar un nombre de usuario
    - Implementada función async confirmUsername para confirmar un nombre de usuario
    - Se registra en la consola el proceso de confirmación del nombre de usuario
    - Utilizada la función writeBatch de Firebase para realizar operaciones de escritura en lote
    - Se establece un nuevo documento en la colección 'usernames' con el nombre de usuario y el UID del usuario actual
    - Se crea un documento en la colección 'users' con detalles adicionales del usuario como nombre de usuario, foto de perfil (si está disponible), estado de publicación, biografía y enlaces
    - Se realiza un commit en lote para ejecutar las operaciones de escritura
    - Reiniciadas las variables de nombre de usuario y disponibilidad para futuros usos

--- src/routes/login/username/+page.svelte
+++ src/routes/login/username/+page.svelte
@@ -28,7 +28,28 @@
         };
 
         async function confirmUsername(){
-            // TODO: 
+            console.log("confirming username", username);
+
+            const batch = writeBatch(db);
+            batch.set(doc(db, "usernames", username), {uid: $user?.uid});
+            batch.set(doc(db, "users", $user!.uid), {
+                username,
+                photoURL: $user?.photoURL ?? null,
+                published: true,
+                bio: "I'm the Walrus",
+                links: [
+                    {
+                        title: "Test Link",
+                        url: "https://kung.foo",
+                        icon: "custom",
+                    }
+                ]
+            });
+
+            await batch.commit();
+
+            username = "";
+            isAvailable = false;
         }
 </script>
 <AuthCheck>
