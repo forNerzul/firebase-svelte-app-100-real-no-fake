b836ee6b1b407eb21411be3670acd7ef8005f34e
forNerzul <sbeardman92@gmail.com>
2023-12-14 15:06:05 -0300

    Agregada funcionalidad para manejar el estado de los datos del usuario
    - Implementada la función genérica `docStore<T>` para gestionar la suscripción a documentos específicos de Firestore.
    - Se utiliza `writable` de Svelte para crear una suscripción a un documento en Firestore con un tipo genérico que puede ser nulo o del tipo especificado.
    - Definida la constante `user` que utiliza un store previamente definido para manejar la autenticación del usuario.
    - Se ha definido una interfaz `UserData` que describe la estructura esperada de los datos del usuario.
    - La constante `userData` utiliza la función `derived` para crear un store que reacciona al cambio del usuario. Si el usuario existe, se subscribe al documento correspondiente en Firestore mediante `docStore`, adaptando los datos al tipo `UserData`. Si el usuario no existe, se establece el valor en `null`.

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,8 +1,8 @@
 import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
+import { getFirestore, doc, onSnapshot } from 'firebase/firestore';
 import { getAuth, onAuthStateChanged, type User } from 'firebase/auth';
 import { getStorage } from 'firebase/storage';
-import { writable } from 'svelte/store';
+import { derived, writable, type Readable } from 'svelte/store';
 
 // configuración de tu aplicación firebase
 const firebaseConfig = {
@@ -21,26 +21,57 @@ export const auth = getAuth();
 export const storage = getStorage();
 
 // store para el firebase user
-function userStore(){
+function userStore() {
 	let unsubscribe: () => void;
 
-	if(!auth || !globalThis.window){
-		console.warn("Auth is not initialized or not in browser")
+	if (!auth || !globalThis.window) {
+		console.warn('Auth is not initialized or not in browser');
 		const { subscribe } = writable<User | null>(null);
 		return {
 			subscribe
-		}
+		};
 	}
 
-	const { subscribe } = writable(auth?.currentUser ?? null,(set) => {
+	const { subscribe } = writable(auth?.currentUser ?? null, (set) => {
 		unsubscribe = onAuthStateChanged(auth, (user) => {
 			set(user);
-		})
+		});
+		return () => unsubscribe();
+	});
+	return {
+		subscribe
+	};
+}
+
+function docStore<T>(path: string) {
+	let unsubscribe: () => void;
+	const docRef = doc(db, path);
+	const { subscribe } = writable<T | null>(null, (set) => {
+		unsubscribe = onSnapshot(docRef, (snapshot) => {
+			set((snapshot.data() as T) ?? null);
+		});
 		return () => unsubscribe();
 	});
 	return {
 		subscribe,
-	}
+		ref: docRef,
+		id: docRef.id
+	};
 }
 
 export const user = userStore();
+
+interface UserData {
+	username: string;
+	bio: string;
+	photoURL: string;
+	links: any[];
+}
+
+export const userData: Readable<UserData | null> = derived(user, ($user, set) => {
+	if ($user) {
+		return docStore<UserData>(`users/${$user.uid}`).subscribe(set);
+	} else {
+		set(null);
+	}
+});
